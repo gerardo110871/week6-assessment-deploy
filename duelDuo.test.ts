28b8fec4fb0a11366713a4a934a6528a73d4c0dc
Gerardo <gpadil@icloud.com>
2022-03-18 15:56:49 -0600

    added some tests on dualDuo

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -17,13 +17,17 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+    await driver.sleep(1000)
+
+    await driver.findElement(By.id('see-all')).click()
+
+    await driver.sleep(3000)
+
+    await driver.findElement(By.id('draw')).click()
+
+    await driver.sleep(3000)
+
+    await driver.findElement(By.className('bot-btn')).click()
+
     await driver.sleep(3000)
 })
-
-test('clicking on draw shows the bot choices', async () => {
-    const title = await driver.findElement(By.id('draw'))
-    const displayed = await title.isDisplayed()
-    expect(displayed).toBe(true)
-    driver.sleep(3000)
-})
-
