e9df67a49b53a670ca7612be1f003f520edc969a
Gerardo <gpadil@icloud.com>
2022-03-19 15:36:35 -0600

    finished the Selenium tests

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,16 +18,36 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
     await driver.sleep(1000)
+})
 
-    await driver.findElement(By.id('see-all')).click()
+test('click see all bots button', async () => {
+    const seeAll = await driver.findElement(By.xpath('//*[@id="see-all"]'))
+    seeAll.click()
+    await driver.sleep(3000)
+})
 
+test('click on draw button', async () => {
+    const draw = await driver.findElement(By.id('draw'))
+    draw.click()
+    await driver.sleep(3000)
+})
+
+test('draw characters, pick two and duel', async () => {
+    const draw = await driver.findElement(By.id('draw'))
+    draw.click()
     await driver.sleep(3000)
 
-    await driver.findElement(By.id('draw')).click()
+    const pickOne = await driver.findElement(By.xpath('//*[@id="choices"]/div[1]/button'))
+    pickOne.click()
 
-    await driver.sleep(3000)
+    await driver.sleep(1000)
 
-    await driver.findElement(By.className('bot-btn')).click()
+    const pickTwo = await driver.findElement(By.xpath('//*[@id="choices"]/div[2]/button'))
+    pickTwo.click()
 
+    await driver.sleep(2000)
+
+    const duel = await driver.findElement(By.xpath('//*[@id="duel"]'))
+    duel.click()
     await driver.sleep(3000)
 })
